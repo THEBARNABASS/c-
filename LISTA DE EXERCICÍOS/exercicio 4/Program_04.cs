6010a73a7004608a1d5e0dcf426dcd2e15a1e328
Cauã De Souza Francelino <57256639813@info.local>
2023-03-31 08:58:24 -0300

    finalizado

---
+++ LISTA DE EXERCICÍOS/exercicio 4/Program_04.cs
@@ -0,0 +1,21 @@
+﻿// 4)Escreva     um     programa     que     verifique     a     validade     de     uma     senha     fornecida pelo     usuário. 
+// A     senha     válida     é     o     número     1234. 
+// Devem     ser    impressas     as    seguintes     mensagens:
+// ACESSO    PERMITIDO    caso    a    senha    seja    válida.
+// ACESSO    NEGADO    caso    a    senha    seja    inválida.
+
+Console.WriteLine($"Usuario:");
+string login = Console.ReadLine();
+
+Console.WriteLine($"Senha:");
+int senha = int.Parse(Console.ReadLine());
+
+if(senha == 1234)
+{
+    Console.WriteLine($"ACESSO PERMITIDO"); 
+}
+else
+{
+    Console.WriteLine($"ACESSO NEGADO");
+    
+}
---
+++ LISTA DE EXERCICÍOS/exercicio 5/Program_05.cs
@@ -0,0 +1,27 @@
+﻿// 5)As    maçãs custam R$ 0,30 cada se forem compradas    menos do que uma dúzia, e R$ 0,25 se forem compradas pelo menos doze. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o valor    total    da compra.
+
+Console.WriteLine($"numero de maçãs a serem compradas:");
+int maca = int.Parse(Console.ReadLine()); 
+
+float valor = 0.30f;
+
+float valor2 = 0.25f;
+
+
+
+if( maca <= 12)
+{
+ float valorfinal = maca * valor;
+ Console.WriteLine("valor da compra:" + valorfinal);
+ 
+}
+else if (maca > 12)
+{
+    float valorfinal = maca * valor2;
+    Console.WriteLine("valor da compra:" + valorfinal);  
+}
+else{
+
+    Console.WriteLine($"numero não válido");
+    
+}
