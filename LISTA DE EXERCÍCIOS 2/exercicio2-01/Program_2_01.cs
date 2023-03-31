e478e9ac861dd5439092f13402facec17de8266a
Cauã De Souza Francelino <57256639813@info.local>
2023-03-31 10:58:16 -0300

    novos exercicios

---
+++ LISTA DE EXERCÍCIOS 2/exercicio2-01/Program_2_01.cs
@@ -0,0 +1,29 @@
+﻿// Escreva um programa que pergunte o dia, mês e ano do aniversário de uma pessoa e diga se a data é válida ou não. Caso não seja, diga o motivo. Suponha que todos os meses tem 31 dias e que estejamos no ano de 2013.
+
+Console.WriteLine($"dia:");
+int dia = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"mês:");
+int mes = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"ano:");
+int ano = int.Parse(Console.ReadLine());
+
+if (dia <= 31 && mes <= 12 && ano <= 2013)
+{
+    Console.WriteLine($"Esta data é valida");
+    
+}
+else if (dia > 31)
+{
+    Console.WriteLine($"Esta data não é valida, o mes contem apenas 31 dias.");
+    
+}
+else if(mes > 12)
+{
+    Console.WriteLine($"Esta data não é valida, o ano contem apenas 12 meses.");
+}
+else if (ano > 2013)
+{
+    Console.WriteLine($"Esta data não é valida, o ano atual é 2013.");
+}
---
+++ LISTA DE EXERCÍCIOS 2/exercicio2_02/Program_2_02.cs
@@ -0,0 +1,57 @@
+﻿// Faça um programa que leia três números e mostre o maior e o menor deles.
+
+Console.WriteLine($"Digite o primeiro numero:");
+int n1 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Digite o segundo numero:");
+int n2 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Digite o terceiro numero:");
+int n3 = int.Parse(Console.ReadLine());
+
+if(n1 > n2 || n1 > n3)
+{
+    Console.WriteLine($"o primeiro numero é maior {n1}");
+    
+
+    if (n2 > n1 || n2 >n3)
+    {
+         Console.WriteLine($"o segundo numero é maior {n2}");
+
+        if (n3 > n1 || n3 > n2)
+       
+             Console.WriteLine($"o primeiro numero é maior {n3}");
+        {
+
+
+
+        
+
+ else if (n1 < n2 || n1 < n3)
+{
+    Console.WriteLine($"o primeiro numero é menor");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        
+    }
+
+}
