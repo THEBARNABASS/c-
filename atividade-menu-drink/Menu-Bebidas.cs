b5d5eed955c4b35ecdd93a471f0b1258d4e8451d
Cauã De Souza Francelino <57256639813@info.local>
2023-04-04 09:01:53 -0300

    Menu Bebidas

---
+++ atividade-menu-drink/Menu-Bebidas.cs
@@ -0,0 +1,147 @@
+﻿// Crie um menu de opções de bebidas e peça para o usuário escolher uma das opções.
+// A depender da opção, exiba uma mensagem se gostaria de acrescentar gelo.
+// Se sim, exiba uma mensagem que a bebida em questão será com adicional de gelo, caso contrário, a bebida é sem gelo adicional.
+
+
+        Console.WriteLine(@$"
+   @===============@
+   | progama menu  |
+   |      de       |
+   |    bebidas    |
+   @===============@
+     ↓ ↓ ↓ ↓ ↓ ↓ ↓ 
+");
+        Console.WriteLine(@$"
+#======================#
+| ********MENU******** |
+#======================#
+| 1. Agua              |
+| -------------------- |
+| 2. Suco              |
+| -------------------- |
+| 3. Refrigerante      |
+| -------------------- |
+| 4. cerveja           |
+| -------------------- |
+| 5. Drinks            |
+#======================#
+ DIGITE O NUMERO ABAIXO:
+  ↓  ↓  ↓  ↓  ↓  ↓  ↓ 
+");
+
+string escolha = Console.ReadLine();
+
+switch (escolha)
+{
+     case "1":
+     Console.WriteLine($"A bebida escolhida foi Agua.");
+     Console.WriteLine($"Acompanha gelo?");
+     string geloagua = Console.ReadLine().ToLower();
+     
+
+ 
+        if(geloagua == "sim")
+        {
+            Console.WriteLine($"Será adicionado gelo a bebida.");  
+        }
+        else
+        {
+            Console.WriteLine($"não será adicionado gelo a bebida.");           
+        }
+
+            Console.WriteLine($"Preparando sua bebida");
+            Console.WriteLine($".........");
+            Console.WriteLine($"pronto! S2");
+     
+     break;
+
+     case "2":
+     Console.WriteLine($"A bebida escolhida foi suco.");
+     Console.WriteLine($"Acompanha gelo?");
+     string gelosuco = Console.ReadLine().ToLower();
+
+      if(gelosuco == "sim")
+        {
+            Console.WriteLine($"Será adicionado gelo a bebida.");  
+        }
+        else
+        {
+            Console.WriteLine($"não será adicionado gelo a bebida.");           
+        }
+
+            Console.WriteLine($"Preparando sua bebida");
+            Console.WriteLine($".........");
+            Console.WriteLine($"pronto! S2");
+     
+     break;
+     
+     case "3":
+     Console.WriteLine($"A bebida escolhida foi suco.");
+     Console.WriteLine($"Acompanha gelo?");
+     string gelorefri = Console.ReadLine().ToLower();
+
+      if(gelorefri == "sim")
+        {
+            Console.WriteLine($"Será adicionado gelo a bebida.");  
+        }
+        else
+        {
+            Console.WriteLine($"não será adicionado gelo a bebida.");           
+        }
+
+            Console.WriteLine($"Preparando sua bebida");
+            Console.WriteLine($".........");
+            Console.WriteLine($"pronto! S2");
+     
+     break;
+     
+    case "4":
+     Console.WriteLine($"A bebida escolhida foi suco.");
+     Console.WriteLine($"Acompanha gelo?");
+     string gelocerveja = Console.ReadLine().ToLower();
+
+      if(gelocerveja == "sim")
+        {
+            Console.WriteLine($"Será adicionado gelo a bebida.");  
+        }
+        else
+        {
+            Console.WriteLine($"não será adicionado gelo a bebida.");           
+        }
+
+            Console.WriteLine($"Preparando sua bebida");
+            Console.WriteLine($".........");
+            Console.WriteLine($"pronto! S2");
+     
+     break;
+     
+     case "5":
+     Console.WriteLine($"A bebida escolhida foi suco.");
+     Console.WriteLine($"Acompanha gelo?");
+     string gelodrinks = Console.ReadLine().ToLower();
+
+      if(gelodrinks == "sim")
+        {
+            Console.WriteLine($"Será adicionado gelo a bebida.");  
+        }
+        else
+        {
+            Console.WriteLine($"não será adicionado gelo a bebida.");           
+        }
+
+            Console.WriteLine($"Preparando sua bebida");
+            Console.WriteLine($".........");
+            Console.WriteLine($"pronto! S2");
+     
+     break;
+
+     default :
+
+     Console.WriteLine($"A opção escolhida é invalida.");
+     break;
+
+
+}
+
+
+    
