5c348bfe638d8d40dea6a16b7f7d38e615cae634
Cauã De Souza Francelino <57256639813@info.local>
2023-04-14 11:00:06 -0300

    um monte de coisa que nao subi kkk

---
+++ LISTA DE EXERCICIOS 4/EXERCICIO_4_04/Program.cs
@@ -0,0 +1,30 @@
+﻿// - Faça um programa que leia 10 valores digitados pelo usuário e no final, escreva o maior e o
+// menor valor lido
+
+int[] nmr = new int[5];
+
+int menor = 0,maior = 0;
+Console.WriteLine($"Informe 10 numeros:");
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Informe o {i+1}º número: ");    
+    nmr[i] = int.Parse(Console.ReadLine());       
+}
+
+for (int i = 0; i < 10; i++)
+{
+    if(nmr[i] < menor )
+    {
+        menor = nmr[i];
+    }
+    else if(nmr[i] > maior)
+    {
+        maior = nmr[i];
+    }
+}
+
+Console.WriteLine($"O menor número é {menor}");
+Console.WriteLine($"O maior número é {maior}");
+
